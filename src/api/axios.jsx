30f7efe4d7bbf01668b9a2c78ab3a20fd9769332
Aniket Raut <rautaniket857@gmail.com>
2026-04-07 20:59:43 +0530

    frontend ready

--- src/api/axios.jsx
+++ src/api/axios.jsx
@@ -2,7 +2,8 @@ import axios from "axios";
 
 const API = axios.create({
   // baseURL: "http://localhost:2001/api",
-  baseURL:"https://todo-app-backend-vn5c.onrender.com"
+  // baseURL:"https://todo-app-backend-vn5c.onrender.com"
+  baseURL: import.meta.env.VITE_API_URL
 });
 
 // Attach token automatically
